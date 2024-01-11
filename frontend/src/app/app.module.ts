7274a202bec19bd72af30cb35883318fe5f0671d
AlbertoPomboParga <148323956+AlbertoPomboParga@users.noreply.github.com>
2024-01-11 12:52:13 +0100

    modificacions contraseña

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,38 +1,44 @@
-import { NgModule } from '@angular/core';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { APP_CONFIG, ONTIMIZE_MODULES, ONTIMIZE_PROVIDERS, OntimizeWebModule } from 'ontimize-web-ngx';
+import { NgModule } from "@angular/core";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import {
+  APP_CONFIG,
+  ONTIMIZE_MODULES,
+  ONTIMIZE_PROVIDERS,
+  OntimizeWebModule,
+} from "ontimize-web-ngx";
 
-import { environment } from '../environments/environment';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CONFIG } from './app.config';
-import { AlertsComponent } from './alerts/alerts.component';
-import { AlertDialogComponent } from './alert-dialog/alert-dialog.component';
+import { environment } from "../environments/environment";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CONFIG } from "./app.config";
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material";
+import { MatDialogModule } from '@angular/material/dialog';
+import { AlertDialogComponent } from "./main/alerts/alert-dialog/alert-dialog.component";
 
 // Standard providers...
 // Defining custom providers (if needed)...
-export const customProviders: any = [
-];
+export const customProviders: any = [];
 
 @NgModule({
   imports: [
     ONTIMIZE_MODULES,
     OntimizeWebModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    MatDialogModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production,
+    }),
   ],
-  declarations: [
-    AppComponent,
-    AlertsComponent,
-    AlertDialogComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ],
+  declarations: [AppComponent, AlertDialogComponent,],
+  bootstrap: [AppComponent],
   providers: [
     { provide: APP_CONFIG, useValue: CONFIG },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: "fill" },
+    },
     ONTIMIZE_PROVIDERS,
-    ...customProviders
-  ],
+  ], 
+  entryComponents: [AlertDialogComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -4,8 +4,7 @@ import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { SharedModule } from '../shared/shared.module';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
-import { AlertsComponent } from './alerts/alerts.component';
-import { AlertDialogComponent } from './alerts/alert-dialog/alert-dialog.component';
+
 
 
 @NgModule({
@@ -16,8 +15,6 @@ import { AlertDialogComponent } from './alerts/alert-dialog/alert-dialog.compone
   ],
   declarations: [
     MainComponent,
-    AlertsComponent,
-    AlertDialogComponent,
   ]
 })
 export class MainModule { }
